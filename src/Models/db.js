249825a93088c86ff156c8b2310975cff1df9a56
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-30 15:11:19 -0800

    test

--- src/Models/db.js
+++ src/Models/db.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const connection = mysql.createConnection({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database:process.env.DB
 })
-
+console.log("Test" + process.env.DB_HOST)
 connection.connect()
 
 connection.query('SELECT * FROM BLOG',(err,rows,fields)=>{
