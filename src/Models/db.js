29da3e18aae321c1762c09b605fe6bee08d06737
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-30 13:16:56 -0800

    test

--- src/Models/db.js
+++ src/Models/db.js
@@ -3,26 +3,23 @@ dotenv.config({path:'../.env'})
 
 const mysql = require('mysql2/promise')
 
-function createConnection(){
-    const connection = mysql.createConnection({
-        host:process.env.DB_HOST,
-        user:process.env.DB_USER,
-        password:process.env.DB_PASSWORD,
-        database:process.env.DB,
-    })
-    connection.promise = () => {
-        return new Promise((resolve,reject) => {
-            connection.connect((err)=>{
-                if(err){
-                    reject(err);
-                }
-                else{
-                    resolve(connection)
-                }
-            })
+async function createConnection(){
+    try{
+        const connection = await mysql.createConnection({
+            host:process.env.DB_HOST,
+            user:process.env.DB_USER,
+            password:process.env.DB_PASSWORD,
+            database:process.env.DB,
         })
+        return connection;
+
     }
-    return connection;
+
+    catch(error){
+        console.error('Error creating MySql connection: ', error);
+        throw error;
+    }
+
 }
 
 module.exports = {
