8bf9e97aa1b6be74b758819c8ce0edbea6609409
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-30 15:20:37 -0800

    test

--- src/app.js
+++ src/app.js
@@ -5,10 +5,11 @@ const app = express()
 const port = process.env.PORT
 
 
+app.get('/',(req,res)=>{
+    res.send(connection)
+})
 
 app.listen(port,()=>{
     console.log(`example app listening on port ${port}`)
 })
-app.get('/',(req,res)=>{
-    res.send(connection)
-})
+
