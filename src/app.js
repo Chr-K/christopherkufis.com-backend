1181f5a9fbdfc67dca6db8e682530e3733325a11
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-30 15:52:37 -0800

    t

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ require('dotenv').config({path:'../.env'})
 const getStuff = require('./Models/db')
 const app = express()
 
+app.listen('localhost')
+
+
 app.get('/hakuna', (req,res)=>{
 console.log("Hello")
 })
