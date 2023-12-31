db113e42b1aab731c56cbeecc839fb11d83d0836
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-30 16:40:11 -0800

    test

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.listen(3000)
 
-app.get('/articles',(req,res)=>{
-    const stuff = getStuff();
+app.get('/articles',async (req,res)=>{
+    const stuff = await getStuff();
     res.send('stuff')
 })
