f640677f501bf12143345cc36ef526a82a53650f
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-30 17:23:23 -0800

    test

--- src/Models/db.js
+++ src/Models/db.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql')
 require('dotenv').config({path:'../.env'})
 const connection = mysql.createConnection({
-    host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database:process.env.DB})
