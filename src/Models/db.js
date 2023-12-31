5a7cb872e5101c065fc7427dcb33b0f1e9edb617
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-30 17:24:08 -0800

    test

--- src/Models/db.js
+++ src/Models/db.js
@@ -1,8 +1,6 @@
 const mysql = require('mysql')
 require('dotenv').config({path:'../.env'})
 const connection = mysql.createConnection({
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
     database:process.env.DB})
 
 async function getStuff(callback){
