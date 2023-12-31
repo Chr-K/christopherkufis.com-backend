d178f0d852bead668f86fc015a6b8abbd632e551
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-30 17:15:27 -0800

    test

--- src/app.js
+++ src/app.js
@@ -2,9 +2,6 @@ const express = require('express')
 const getStuff = require('./Models/db')
 const app = express()
 
-
-app.use(getStuff)
-
 app.listen(3000)
 
 app.get('/articles', async (req,res)=>{
