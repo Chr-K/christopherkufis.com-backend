7268822f10e990125a226b4a24a5f604fb984a6b
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-31 15:58:52 -0800

    test

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,14 @@ const express = require('express')
 const router = express.Router()
 const articleController = require('./Controllers/articleCont')
 
-router.get('/articles', async (req,res)=>{articleController.getArticles})
+router.get('/articles', async (req,res)=>{
+    try{
+        const articles = await articleController.getArticles()
+        res.send(articles)
+    }
+    catch(err){
+        console.error(err)
+    }
+})
 
 module.exports = router
