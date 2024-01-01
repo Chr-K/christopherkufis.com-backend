60a6021c75de16649bdefac28dd83ad109b08959
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 09:16:15 -0800

    test

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,8 @@ const passport = require('passport')
 const router = express.Router()
 const articleController = require('./Controllers/articleCont')
 router.get('/articles', articleController.getArticles)
-router.post('/auth',(req,res)=>{
-    res.send("boop")
-})
-/*
 router.post('/auth',passport.authenticate('local',{
 successRedirect:'https://cp.christopherkufis.com/home',
 failureRedirect:'https://cp.christopherkufis.com/'
 }))
-*/
 module.exports = router
