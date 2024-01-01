f9dbd7aa4224f5ab0fc3ca5990c56c94c92266e0
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 11:26:03 -0800

    test

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
-console.log('hi')
-
 const express = require('express')
 const session = require('express-session')
-const logger = require('morgan')
 var passport = require('passport');
-console.log('hi')
 const SQLiteStore = require('connect-sqlite3')
 const app = express()
 const routes = require('./routes')
 const bodyParser = require('body-parser')
 const auth = require('./auth')
+
 auth.Authenticate()
+
 app.use(bodyParser.json({ extended: true }));
 
 app.use(session({
