d551c4f2d63bbcd21e02220245bac644ae1a0da5
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 06:21:02 -0800

    test

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const passport = require('passport')
 const router = express.Router()
 const articleController = require('./Controllers/articleCont')
-const adminController = require('./Controllers/adminCont')
 router.get('/articles', articleController.getArticles)
 router.post('/auth',passport.authenticate('local',()=>{
     console.log("hola")
