56b3e576379da9467df7c5a9c776797d4bda940f
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-31 17:37:18 -0800

    test

--- src/Models/auth.js
+++ src/Models/auth.js
@@ -4,19 +4,8 @@ var LocalStrategy = require('passport-local')
 var crypto = require('crypto')
 
 async function Authenticate(user,req){
-    console.log(user)
-    passport.use(new LocalStrategy(function verify(user,password,cb){
-        console.log(user)
-        crypto.pbkdf2(password,row.salt,310000,32,'sha256',function(err,hashedPassword){
-            if(err){return cb(err)}
-            if(!crypto.timingSafeEqual(row.hassedPassword,hashedPassword)){
-                return cb(null,fall,{message:'incorrect login info'})
-            }
-            return cb(null,row)
-        })
-    }))
+    console.log("USER:"+ user + "\n req" + req.body)
 }
-
 
 module.exports={
     Authenticate,
