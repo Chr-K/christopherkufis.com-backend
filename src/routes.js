6b9cfdad966f47ade268701c2923388b120f56cd
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 12:00:51 -0800

    test

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,14 @@ const passport = require('passport')
 const router = express.Router()
 const articleController = require('./Controllers/articleCont')
 router.get('/articles', articleController.getArticles)
-router.post('/auth',passport.authenticate('local',{
-successRedirect:'https://cp.christopherkufis.com/home',
-failureRedirect:'https://cp.christopherkufis.com/',
-}))
+router.post('/auth',(req,res,next)=>{
+    passport.authenticate('local',(err,user,info)=>{
+        if(err){return next(err)}
+        if(!user){
+            return res.status(401).json({Message:"Authentication has failed"})
+        }
+
+        return res.status(200).json({message:"Welcome"})
+    })(req,res,next)
+})
 module.exports = router
