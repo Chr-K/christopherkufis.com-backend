3c96981499136487a24ebcf41cc4e5dac544e3cb
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 11:33:30 -0800

    test

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const session = require('express-session')
 const logger = require('morgan')
-const SQLiteStore = require('connect-sqlite3')
 const app = express()
 const routes = require('./routes')
 const bodyParser = require('body-parser')
 const auth = require('./auth')
+
+var SQLiteStore = require('connect-sqlite3')(session);
+
 auth.Authenticate()
 app.use(bodyParser.json({ extended: true }));
 
@@ -13,7 +15,7 @@ app.use(session({
     secret:'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    store: new SQLiteStore({db:'sessions.db',dir:'/'})
+    store: new SQLiteStore({db:'sessions.db',dir:'/var/db'})
 }))
 app.use('/',routes)
 app.listen(3000)
