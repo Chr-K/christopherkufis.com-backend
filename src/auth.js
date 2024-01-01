fc090c6c9fd2e17aa6f67559c1abe7e2c87ea601
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 10:57:58 -0800

    test

--- src/auth.js
+++ src/auth.js
@@ -10,7 +10,7 @@ async function Authenticate(){
           if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
           crypto.pbkdf2(password, row[0].salt, 310000, 64, 'sha256', function(err, hashedPassword){
             console.log(hashedPassword)
-            console.log((Buffer.from(JSON.stringify(hashedPassword))).toString('hex'))
+            console.log((Buffer.from(JSON.stringify(hashedPassword))).toString('utf8'))
 
             if (err) {return cb(err);}
             if (!crypto.timingSafeEqual(row[0].PASSWORD, hashedPassword)) {
