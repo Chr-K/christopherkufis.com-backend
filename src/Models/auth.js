d812f1df1d4371e2856e82724c913f70ab01bf38
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-31 16:33:59 -0800

    test

--- src/Models/auth.js
+++ src/Models/auth.js
@@ -1,5 +1,3 @@
-
-
 async function Authenticate(){
 return ("kekw");
 }
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const app = express()
 const routes = require('./routes')
-
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     secret:'secret',
