5e54a769a7fd5ee257b096b2cf78c7649d620159
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 06:19:00 -0800

    test

--- src/auth.js
+++ src/auth.js
@@ -8,7 +8,7 @@ async function Authenticate(){
         connection.query('SELECT * FROM USER WHERE USERNAME = ?', [ username ], function(err, row) {
           if (err) { return cb(err); }
           if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
-          crypto.pbkdf2(password, row[0].salt, 310000, 32, 'sha256', function(err, hashedPassword){
+          crypto.pbkdf2(password, row[0].salt, 310000, 64, 'sha256', function(err, hashedPassword){
             console.log(hashedPassword.length)
             console.log(Buffer.from(row[0].PASSWORD).length)
             if (err) { return cb(err); }
