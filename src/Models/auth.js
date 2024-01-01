1177191bb2a151b12ae8cc57b194c5dcf08d80a2
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-31 18:08:29 -0800

    test

--- src/Models/auth.js
+++ src/Models/auth.js
@@ -3,11 +3,14 @@ var LocalStrategy = require('passport-local')
 var crypto = require('crypto')
 
 async function Authenticate(user,req){
-    console.log('schleem')
     passport.use(new LocalStrategy(function verify(cb) {
+        console.log('schleem')
           crypto.pbkdf2(req.body["PASSWORD"], user.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+            console.log('schlop')
             if (err) { return cb(err); }
+            console.log("ping")
             if (!crypto.timingSafeEqual(user.hashed_password, hashedPassword)) {
+                console.log('pang')
               return cb(null, false, { message: 'Incorrect username or password.' });
             }
             console.log('chaching')
