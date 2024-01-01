e86149915d6dfe2c22d26407c4cd1bb495646d06
Chr-K <104398517+Chr-K@users.noreply.github.com>
2023-12-31 17:49:07 -0800

    test

--- src/Models/auth.js
+++ src/Models/auth.js
@@ -4,7 +4,7 @@ var LocalStrategy = require('passport-local')
 var crypto = require('crypto')
 
 async function Authenticate(user,req){
-    console.log("USER:"+ user.USERNAME + "\n req: " + req.body['USERNAME'])
+    console.log("USER:"+ user + "\n req: " + req.body['USERNAME'])
 }
 
 module.exports={
