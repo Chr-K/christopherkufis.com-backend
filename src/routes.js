e3427c7ade897dde41baa702a58f4a39aef0e368
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 17:00:05 -0800

    test

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const articleController = require('./Controllers/articleCont')
 router.get('/articles', articleController.getArticles)
 router.get('/article',(req,res,next)=>{
+    console.log("hi")
     articleController.getArticle(req,res)
 })
 
