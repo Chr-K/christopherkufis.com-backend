a3e1c85f66141f6711c24502d9315c937857aeac
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 17:01:55 -0800

    test

--- src/Controllers/articleCont.js
+++ src/Controllers/articleCont.js
@@ -12,6 +12,7 @@ async function getArticles(req,res){
 }
 async function getArticle(req,res){
     try{
+        console.log(req)
         const article = await articleModel.getArticle(req.body.ID)
         res.send(article)
     }
