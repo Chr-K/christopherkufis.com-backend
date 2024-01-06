a5873a6a289c29e9c1d973c234241bf3c6a322ed
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-06 11:21:00 -0800

    test

--- src/routes.js
+++ src/routes.js
@@ -25,7 +25,7 @@ router.post('/auth',(req,res,next)=>{
 })
 
 router.post('/submitarticle',(req,res,next)=>{
-    console.log(req.session.isAuthenticated())
+    console.log(req.cookies)
     if(req.isAuthenticated()){
         res.status(200).json('is logged in')
     }
