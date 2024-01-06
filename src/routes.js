99617b6b82f8eb5b094098e8b631b87da5d4ac2e
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-06 09:07:31 -0800

    test

--- src/routes.js
+++ src/routes.js
@@ -17,7 +17,7 @@ router.post('/auth',(req,res,next)=>{
         if(!user){
             return res.status(401).json({Message:"Authentication has failed"})
         }
-        console.log(req.session.username)
+        console.log(req.session)
         return res.status(200).json({message:"Welcome"})
     })(req,res,next)
 
