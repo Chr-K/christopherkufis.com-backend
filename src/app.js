5aa1e6937c9bbf3bb342666302304adb5330fa71
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-06 09:11:32 -0800

    test

--- src/app.js
+++ src/app.js
@@ -36,6 +36,18 @@ app.use(session({
 }))
 app.use(passport.session())
 
+passport.serializeUser(function(user, cb){
+    process.nextTick(function() {
+      cb(null, { id: user.id, username: user.username });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
 auth.Authenticate()
 
 app.use('/',routes)
