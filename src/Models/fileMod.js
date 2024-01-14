8db43611ebcb3c424cd2ff0fff8619bcf91c3e32
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-14 15:25:55 -0800

    test

--- src/Models/fileMod.js
+++ src/Models/fileMod.js
@@ -6,11 +6,8 @@ const path = process.env.ARTICLE_IMAGE_PATH
 
 
 async function uploadImage(req,res){
-    let name = Date.now()
-    fs.appendFile()
 
     res.status(200).json(req.file.mimetype)
-
 }
 
 module.exports = {
