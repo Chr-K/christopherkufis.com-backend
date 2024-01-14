93e573fb1549dc98c64d11e9c254684d34a2c34f
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-14 15:41:26 -0800

    test

--- src/Models/fileMod.js
+++ src/Models/fileMod.js
@@ -1,11 +1,21 @@
 const fs = require('fs')
+const multer = require('multer')
 require('dotenv').config({path:'../.env'})
 
 const path = process.env.ARTICLE_IMAGE_PATH
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,path)
+    },
+    filename:(req,file,cb)=>{
+        cb(null,Date.now())
+    }
+})
 
+const upload = storage({storage:storage})
 
 async function uploadImage(req,res){
-
+console.log(req.file.mimetype)
 }
 
 module.exports = {
